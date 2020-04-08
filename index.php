40ad4a8156aa4bfed4fc3823bb15b309d9e95599
Yappiyapi <yynissy.718@gmail.com>
2020-04-08 18:53:05 +0900

    ヘッダー・フッター作成

--- index.php
+++ index.php
@@ -3,11 +3,54 @@
 require_once('config.php');
 require_once('functions.php');
 
-try {
-  $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
-  echo '接続に成功しました！' . '<br>';
-} catch (PDOException $e) {
-  // 接続がうまくいかない場合こちらの処理がなされる
-  echo $e->getMessage();
-  exit;
-}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>pelicula</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="flex-col-area">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 shadow">
+      <a href="index.php" class="navbar-brand">
+        <h2>
+          Pelicula
+          <svg class="bi bi-film" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M0 1a1 1 0 011-1h14a1 1 0 011 1v14a1 1 0 01-1 1H1a1 1 0 01-1-1V1zm4 0h8v6H4V1zm8 8H4v6h8V9zM1 1h2v2H1V1zm2 3H1v2h2V4zM1 7h2v2H1V7zm2 3H1v2h2v-2zm-2 3h2v2H1v-2zM15 1h-2v2h2V1zm-2 3h2v2h-2V4zm2 3h-2v2h2V7zm-2 3h2v2h-2v-2zm2 3h-2v2h2v-2z" clip-rule="evenodd" />
+          </svg>
+        </h2>
+      </a>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a href="sign_in.php" class="nav-link">ログイン</a>
+          </li>
+          <li class="nav-item">
+            <a href="sign_up.php" class="nav-link">アカウント登録</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <footer class="footer font-small bg-light">
+      <div class="footer-copyright text-center py-3 text-dark">&copy; 2020 Pelicula</div>
+    </footer>
+  </div>
+</body>
+
+</html>
